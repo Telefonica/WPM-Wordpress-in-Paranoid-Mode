eef93db83ece1c61360d0faa0a30eb56dab5cf2f
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2016-06-21 13:17:03 -0300

    Getting OS Information to the file install.sh

--- WPM_Latch/install.sh
+++ WPM_Latch/install.sh
@@ -27,6 +27,33 @@ then
 	exit 1
 fi
 
+### Getting OS Information
+if [ -f /etc/lsb-release ]; 
+then
+	. /etc/lsb-release
+	DIST=$DISTRIB_ID
+	DIST_VER=$DISTRIB_RELEASE
+else
+	DIST="Unknown"
+	DIST_VER="Unknown"
+fi
+if [ -f /etc/debian_version ]; 
+then
+	OS="Debian"
+	VER=$(cat /etc/debian_version)
+elif [ -f /etc/redhat-release ]; 
+then
+	OS="Red Hat"
+	VER=$(cat /etc/redhat-release)
+elif [ -f /etc/SuSE-release ]; 
+then
+	OS="SuSE"
+	VER=$(cat /etc/SuSE-release)
+else
+	OS=$(uname -s)
+	VER=$(uname -r)
+fi
+
 which_cmd() {
 	local block=1
 	if [ "a${1}" = "a-n" ]
